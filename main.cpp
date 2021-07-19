3967054057f1cacbda6d74fd5ce1559fb1827038
mmjvox <mmdjv.kh94@gmail.com>
2021-07-19 14:38:11 +0430

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <QCoreApplication>
+
+#include "xlsxdocument.h"
+#include "xlsxchartsheet.h"
+#include "xlsxcellrange.h"
+#include "xlsxchart.h"
+#include "xlsxrichstring.h"
+#include "xlsxworkbook.h"
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QXlsx::Document xlsxR;
+    xlsxR.readExcelFile("./Datalistsss.xlsx");
+
+    qDebug()<<xlsxR.sheetNames();
+
+    qDebug()<<xlsxR.selectSheet("DATA");
+
+    return a.exec();
+}
